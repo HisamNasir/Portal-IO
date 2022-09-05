9b6139d40af6bc80e40f94894c88f98e89960561
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2022-09-05 19:09:02 +0500

    Add files via upload

---
+++ Portal.IO/Portal.IO/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Portal.IO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LOLbtn = new System.Windows.Forms.Button();
+            this.Playlistbtn = new System.Windows.Forms.Button();
+            this.FBbtn = new System.Windows.Forms.Button();
+            this.Blitzbtn = new System.Windows.Forms.Button();
+            this.BlackBoardbtn = new System.Windows.Forms.Button();
+            this.Openallbtn = new System.Windows.Forms.Button();
+            this.Shutbtn = new System.Windows.Forms.Button();
+            this.Taskbtn = new System.Windows.Forms.Button();
+            this.TopPnl = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TopPnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LOLbtn
+            // 
+            this.LOLbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.LOLbtn.FlatAppearance.BorderSize = 0;
+            this.LOLbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LOLbtn.Image = global::Portal.IO.Properties.Resources.icons8_league_of_legends_48;
+            this.LOLbtn.Location = new System.Drawing.Point(263, 146);
+            this.LOLbtn.Name = "LOLbtn";
+            this.LOLbtn.Size = new System.Drawing.Size(88, 88);
+            this.LOLbtn.TabIndex = 0;
+            this.LOLbtn.UseVisualStyleBackColor = false;
+            this.LOLbtn.Click += new System.EventHandler(this.LOLbtn_Click);
+            // 
+            // Playlistbtn
+            // 
+            this.Playlistbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Playlistbtn.FlatAppearance.BorderSize = 0;
+            this.Playlistbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Playlistbtn.Image = global::Portal.IO.Properties.Resources.icons8_playlist_48;
+            this.Playlistbtn.Location = new System.Drawing.Point(357, 146);
+            this.Playlistbtn.Name = "Playlistbtn";
+            this.Playlistbtn.Size = new System.Drawing.Size(88, 88);
+            this.Playlistbtn.TabIndex = 0;
+            this.Playlistbtn.UseVisualStyleBackColor = false;
+            this.Playlistbtn.Click += new System.EventHandler(this.Playlistbtn_Click);
+            // 
+            // FBbtn
+            // 
+            this.FBbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.FBbtn.FlatAppearance.BorderSize = 0;
+            this.FBbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FBbtn.Image = global::Portal.IO.Properties.Resources.icons8_facebook_48__1_;
+            this.FBbtn.Location = new System.Drawing.Point(357, 240);
+            this.FBbtn.Name = "FBbtn";
+            this.FBbtn.Size = new System.Drawing.Size(88, 88);
+            this.FBbtn.TabIndex = 0;
+            this.FBbtn.UseVisualStyleBackColor = false;
+            this.FBbtn.Click += new System.EventHandler(this.FBbtn_Click);
+            // 
+            // Blitzbtn
+            // 
+            this.Blitzbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Blitzbtn.FlatAppearance.BorderSize = 0;
+            this.Blitzbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Blitzbtn.Image = global::Portal.IO.Properties.Resources.icons8_lightning_bolt_48__2_;
+            this.Blitzbtn.Location = new System.Drawing.Point(263, 240);
+            this.Blitzbtn.Name = "Blitzbtn";
+            this.Blitzbtn.Size = new System.Drawing.Size(88, 88);
+            this.Blitzbtn.TabIndex = 0;
+            this.Blitzbtn.UseVisualStyleBackColor = false;
+            this.Blitzbtn.Click += new System.EventHandler(this.Blitzbtn_Click);
+            // 
+            // BlackBoardbtn
+            // 
+            this.BlackBoardbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BlackBoardbtn.FlatAppearance.BorderSize = 0;
+            this.BlackBoardbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BlackBoardbtn.Image = global::Portal.IO.Properties.Resources.icons8_blackboard_48;
+            this.BlackBoardbtn.Location = new System.Drawing.Point(451, 146);
+            this.BlackBoardbtn.Name = "BlackBoardbtn";
+            this.BlackBoardbtn.Size = new System.Drawing.Size(88, 88);
+            this.BlackBoardbtn.TabIndex = 0;
+            this.BlackBoardbtn.UseVisualStyleBackColor = false;
+            this.BlackBoardbtn.Click += new System.EventHandler(this.BlackBoardbtn_Click);
+            // 
+            // Openallbtn
+            // 
+            this.Openallbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Openallbtn.FlatAppearance.BorderSize = 0;
+            this.Openallbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Openallbtn.Image = global::Portal.IO.Properties.Resources.icons8_all_out_48;
+            this.Openallbtn.Location = new System.Drawing.Point(451, 240);
+            this.Openallbtn.Name = "Openallbtn";
+            this.Openallbtn.Size = new System.Drawing.Size(88, 88);
+            this.Openallbtn.TabIndex = 0;
+            this.Openallbtn.UseVisualStyleBackColor = false;
+            this.Openallbtn.Click += new System.EventHandler(this.Openallbtn_Click);
+            // 
+            // Shutbtn
+            // 
+            this.Shutbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Shutbtn.FlatAppearance.BorderSize = 0;
+            this.Shutbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Shutbtn.Image = global::Portal.IO.Properties.Resources.icons8_shutdown_55;
+            this.Shutbtn.Location = new System.Drawing.Point(681, 350);
+            this.Shutbtn.Name = "Shutbtn";
+            this.Shutbtn.Size = new System.Drawing.Size(88, 88);
+            this.Shutbtn.TabIndex = 0;
+            this.Shutbtn.UseVisualStyleBackColor = false;
+            this.Shutbtn.Click += new System.EventHandler(this.Shutbtn_Click);
+            // 
+            // Taskbtn
+            // 
+            this.Taskbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Taskbtn.FlatAppearance.BorderSize = 0;
+            this.Taskbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Taskbtn.Image = global::Portal.IO.Properties.Resources.icons8_system_task_55;
+            this.Taskbtn.Location = new System.Drawing.Point(25, 350);
+            this.Taskbtn.Name = "Taskbtn";
+            this.Taskbtn.Size = new System.Drawing.Size(88, 88);
+            this.Taskbtn.TabIndex = 0;
+            this.Taskbtn.UseVisualStyleBackColor = false;
+            this.Taskbtn.Click += new System.EventHandler(this.Taskbtn_Click);
+            // 
+            // TopPnl
+            // 
+            this.TopPnl.Controls.Add(this.label1);
+            this.TopPnl.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TopPnl.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.TopPnl.Location = new System.Drawing.Point(0, 0);
+            this.TopPnl.Name = "TopPnl";
+            this.TopPnl.Size = new System.Drawing.Size(804, 90);
+            this.TopPnl.TabIndex = 1;
+            this.TopPnl.Paint += new System.Windows.Forms.PaintEventHandler(this.TopPnl_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(336, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Portal IO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(33)))));
+            this.BackgroundImage = global::Portal.IO.Properties.Resources._1280x800_stroke_minimalism_4k_1539371317;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TopPnl);
+            this.Controls.Add(this.Taskbtn);
+            this.Controls.Add(this.Shutbtn);
+            this.Controls.Add(this.Blitzbtn);
+            this.Controls.Add(this.Openallbtn);
+            this.Controls.Add(this.BlackBoardbtn);
+            this.Controls.Add(this.FBbtn);
+            this.Controls.Add(this.Playlistbtn);
+            this.Controls.Add(this.LOLbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TopPnl.ResumeLayout(false);
+            this.TopPnl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LOLbtn;
+        private System.Windows.Forms.Button Playlistbtn;
+        private System.Windows.Forms.Button FBbtn;
+        private System.Windows.Forms.Button Blitzbtn;
+        private System.Windows.Forms.Button BlackBoardbtn;
+        private System.Windows.Forms.Button Openallbtn;
+        private System.Windows.Forms.Button Shutbtn;
+        private System.Windows.Forms.Button Taskbtn;
+        private System.Windows.Forms.Panel TopPnl;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Portal.IO/Portal.IO/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace Portal.IO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void TopPnl_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void LOLbtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Riot Games\League of Legends\LeagueClient");
+        }
+
+        private void Playlistbtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"https://www.youtube.com/watch?v=SXF-Eu8XwC8&list=PLQEgoqAxuULrgl8QTy9_Q9kxUa1AaIdMo");
+        }
+
+        private void BlackBoardbtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"https://iqra.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1");
+        }
+        private void Blitzbtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\Hisam\AppData\Local\Programs\Blitz\Blitz");
+        }
+
+        private void FBbtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"https://www.facebook.com/");
+        }
+        private void Openallbtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"https://www.youtube.com/watch?v=SXF-Eu8XwC8&list=PLQEgoqAxuULrgl8QTy9_Q9kxUa1AaIdMo");
+            System.Diagnostics.Process.Start(@"C:\Windows\System32\Taskmgr");
+            System.Diagnostics.Process.Start(@"C:\Users\Hisam\AppData\Local\Programs\Blitz\Blitz");
+            System.Diagnostics.Process.Start(@"C:\Riot Games\League of Legends\LeagueClient");
+        }
+
+        private void Taskbtn_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Process.Start(@"C:\Windows\System32\Taskmgr");
+        }
+
+        private void Shutbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+    }
+}
